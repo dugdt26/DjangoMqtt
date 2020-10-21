891ae8d178107b6ded5f4ed18726404edfab579b
Dũng Đỗ <tiendung.ict.hust@gmail.com>
2020-10-21 20:35:46 +0700

    post data to db
    post data to db

--- mysite/mqtt_client/mqtt.py
+++ mysite/mqtt_client/mqtt.py
@@ -1,17 +1,27 @@
 import paho.mqtt.client as mqtt
-topic = "mqtt/demo"
-def on_conect(client,userdata,flags, rc):
+#topic = "mqtt/demo"
+topic = "ThuatDH/topic"
+
+
+def on_conect(client, userdata, flags, rc):
     client.subscribe(topic)
 
-def on_messenge(client,userdata, msg):
+
+def on_messenge(client, userdata, msg):
     import json
     from.models import PostMqtt
     if(msg.topic == topic):
-        print(msg.payload)
-client =mqtt.Client("mqtt_demo")
+
+        data = json .loads(msg.payload.decode("utf8"))
+        tmp = PostMqtt.objects.create(
+            # temp=data["temp"], hud=data["hud"], pm1=data["pm1"], pm2=data["pm2"], pm3=data["pm3"])
+            temp=data["Env_temp"], hud=data["Env_humidity"], pm1=1, pm2=2, pm3=3)
+        tmp.save()
+       # print(msg.payload)
+
+
+client = mqtt.Client()
 client.on_connect = on_conect
 client.on_message = on_messenge
 
 client.connect("mqtt.eclipse.org", 1883, 60)
-
-
--- mysite/publisher.py
+++ mysite/publisher.py
@@ -1,10 +1,12 @@
 import paho.mqtt.client as mqtt
+import json
 
 client_name = "Publisher"
 topic = "mqtt/demo"
-msg = "Hello"
-
+msg = {"temp":30, "hud":60, "pm1": 355, "pm2":50, "pm3":100}
+# data = json.loads(msg)
+data = json.dumps(msg)
 client =mqtt.Client(client_name)
 client.connect("mqtt.eclipse.org", 1883, 60)
 
-client.publish(topic, msg)
+client.publish(topic, data)
